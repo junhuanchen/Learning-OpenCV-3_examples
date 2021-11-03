af45e0b55d2c17a201540b209a1641fa94336ab3
junhuanchen <junhuanchen@qq.com>
2021-11-03 18:22:13 +0800

    [update]

--- Exercises_13_1-2-11.cpp
+++ Exercises_13_1-2-11.cpp
@@ -35,108 +35,114 @@ int main( int argc, const char** argv )
 	e.  Report what you see with high settings between 200 and 250.
 	f.  Summarize your results and explain what happens as best you can.                                                                     */
 	/************************************************************************/
-  Mat src = imread(argv[1],IMREAD_GRAYSCALE);
-	Mat dst;
-	
-	int iHighThresh = 50;
-	Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
-	imshow("iHighThresh is 50 and  high:low is 1.5:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
-	imshow("iHighThresh is 50 and  high:low is 2.75:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
-	imshow("iHighThresh is 50 and  high:low is 4:1",dst);
+  {
 
-	iHighThresh = (50+100)/2;
-	Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
-	imshow("iHighThresh is (50+100)/2 and  high:low is 1.5:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
-	imshow("iHighThresh is (50+100)/2 and  high:low is 2.75:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
-	imshow("iHighThresh is (50+100)/2 and  high:low is 4:1",dst);
+    cv::Mat src = imread(argv[1],IMREAD_GRAYSCALE);
+    cv::Mat dst;
 
-	iHighThresh = (100+150)/2;
-	Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
-	imshow("iHighThresh is (100+150)/2 and  high:low is 1.5:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
-	imshow("iHighThresh is (100+150)/2 and  high:low is 2.75:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
-	imshow("iHighThresh is (100+150)/2 and  high:low is 4:1",dst);
+    int iHighThresh = 50;
+    Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
+    imshow("iHighThresh is 50 and  high:low is 1.5:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
+    imshow("iHighThresh is 50 and  high:low is 2.75:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
+    imshow("iHighThresh is 50 and  high:low is 4:1",dst);
 
-	iHighThresh = (150+200)/2;
-	Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
-	imshow("iHighThresh is (150+200)/2 and  high:low is 1.5:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
-	imshow("iHighThresh is (150+200)/2 and  high:low is 2.75:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
-	imshow("iHighThresh is (150+200)/2 and  high:low is 4:1",dst);
+    iHighThresh = (50+100)/2;
+    Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
+    imshow("iHighThresh is (50+100)/2 and  high:low is 1.5:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
+    imshow("iHighThresh is (50+100)/2 and  high:low is 2.75:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
+    imshow("iHighThresh is (50+100)/2 and  high:low is 4:1",dst);
 
-	iHighThresh = (200+250)/2;
-	Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
-	imshow("iHighThresh is (200+250)/2 and  high:low is 1.5:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
-	imshow("iHighThresh is (200+250)/2 and  high:low is 2.75:1",dst);
-	Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
-	imshow("iHighThresh is (200+250)/2 and  high:low is 4:1",dst);
+    iHighThresh = (100+150)/2;
+    Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
+    imshow("iHighThresh is (100+150)/2 and  high:low is 1.5:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
+    imshow("iHighThresh is (100+150)/2 and  high:low is 2.75:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
+    imshow("iHighThresh is (100+150)/2 and  high:low is 4:1",dst);
 
+    iHighThresh = (150+200)/2;
+    Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
+    imshow("iHighThresh is (150+200)/2 and  high:low is 1.5:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
+    imshow("iHighThresh is (150+200)/2 and  high:low is 2.75:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
+    imshow("iHighThresh is (150+200)/2 and  high:low is 4:1",dst);
+
+    iHighThresh = (200+250)/2;
+    Canny(src,dst,static_cast<int>(iHighThresh/1.5),iHighThresh);
+    imshow("iHighThresh is (200+250)/2 and  high:low is 1.5:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/2.75),iHighThresh);
+    imshow("iHighThresh is (200+250)/2 and  high:low is 2.75:1",dst);
+    Canny(src,dst,static_cast<int>(iHighThresh/4),iHighThresh);
+    imshow("iHighThresh is (200+250)/2 and  high:low is 4:1",dst);
+
+  }
 	/************************************************************************/
 	/* 2.  Load an image containing clear lines and circles such as a side view of a bicycle.
 	Use  the  Hough  line  and  Hough  circle  calls  and  see  how  they  respond  to  your
 	image.                                                                     */
 	/************************************************************************/
-	Mat src = imread(argv[1],IMREAD_GRAYSCALE);//a bike in gray
-	GaussianBlur( src, src, Size(9, 9), 2, 2 );
-	Mat temp;
-	vector<Vec4i> linesP;
-	vector<Vec3f> circles;
-	//first find the canny edge
-	Canny(src,temp,50,200);
-	//find lines
-	HoughLinesP(temp,linesP,1,CV_PI/180,80,50,10);
-	//find circles
-	HoughCircles( src, circles, CV_HOUGH_GRADIENT, 1, src.rows/8, 200, 100, 0, 0 );
-	//draw lines and circles in the source image
-	for (int i = 0;i<linesP.size();i++)
 	{
-		Vec4i l = linesP[i];
-		line(src,Point(l[0],l[1]),Point(l[2],l[3]),Scalar(255));
-	}
 
-	for( size_t i = 0; i < circles.size(); i++ )
-	{
-		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-		int radius = cvRound(circles[i][2]);
-		// circle center
-		circle( src, center, 3, Scalar(255,255,255), -1, 8, 0 );
-		// circle outline
-		circle( src, center, radius, Scalar(255,255,255), 1, 8, 0 );
-	}
-	
-	/*11.  Create an image with a 20 × 20 square in it. Rotate it to an arbitrary angle. Take a
-	distance transform of this image. Create a 20 × 20 square shape. Use the distance
-	transform image to algorithmically overlay the shape onto the rotated square in
-	the image you made.*/
-	// create a 20 × 20 square
-	Mat matBoard = Mat(500,500,CV_8UC3,Scalar(0));
-	rectangle(matBoard,Rect(250,250,20,20),Scalar(255,255,255),-1);
-	// create an arbitary angle
-	cv::RNG rng = cv::theRNG();
-	float fDegree = rng.uniform(0,360);
-	Mat rotMatS = getRotationMatrix2D(Point(250,250), fDegree, 1.0);
-	warpAffine(matBoard, matBoard, rotMatS, matBoard.size(), 1, 0, Scalar(0,0,0));
-	//solove the problem
-	Mat matDistance;
-	cvtColor(matBoard,gray,COLOR_BGR2GRAY);
-	threshold(gray,gray,100,255,CV_THRESH_BINARY); 
-	distanceTransform(gray,matDistance,CV_DIST_L2,3);  
-       //find the center corners
-	double minVal; 
-	double maxVal2; 
-	Point minLoc,maxLoc;
-	minMaxLoc( matDistance, &minVal, &maxVal2, &minLoc, &maxLoc, matDistance>0); 
-	//draw the result
-	circle(matBoard,maxLoc,1,Scalar(0),1);
-	circle(matBoard,minLoc,2,Scalar(255),2);
-	rectangle(matBoard,Rect(maxLoc.x - 10,maxLoc.y - 10,20,20),Scalar(0,0,255),-1);
+    cv::Mat gray = imread(argv[1],IMREAD_GRAYSCALE);//a bike in gray
+    GaussianBlur( gray, gray, Size(9, 9), 2, 2 );
+    Mat temp;
+    vector<Vec4i> linesP;
+    vector<Vec3f> circles;
+    //first find the canny edge
+    Canny(gray,temp,50,200);
+    //find lines
+    HoughLinesP(temp,linesP,1,CV_PI/180,80,50,10);
+    //find circles
+    HoughCircles( gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows/8, 200, 100, 0, 0 );
+    //draw lines and circles in the source image
+    for (int i = 0;i<linesP.size();i++)
+    {
+      Vec4i l = linesP[i];
+      line(gray,Point(l[0],l[1]),Point(l[2],l[3]),Scalar(255));
+    }
+
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( gray, center, 3, Scalar(255,255,255), -1, 8, 0 );
+      // circle outline
+      circle( gray, center, radius, Scalar(255,255,255), 1, 8, 0 );
+    }
+
+    /*11.  Create an image with a 20 × 20 square in it. Rotate it to an arbitrary angle. Take a
+    distance transform of this image. Create a 20 × 20 square shape. Use the distance
+    transform image to algorithmically overlay the shape onto the rotated square in
+    the image you made.*/
+    // create a 20 × 20 square
+    Mat matBoard = Mat(500,500,CV_8UC3,Scalar(0));
+    rectangle(matBoard,Rect(250,250,20,20),Scalar(255,255,255),-1);
+    // create an arbitary angle
+    cv::RNG rng = cv::theRNG();
+    float fDegree = rng.uniform(0,360);
+    Mat rotMatS = getRotationMatrix2D(Point(250,250), fDegree, 1.0);
+    warpAffine(matBoard, matBoard, rotMatS, matBoard.size(), 1, 0, Scalar(0,0,0));
+    //solove the problem
+    Mat matDistance;
+    cvtColor(matBoard,gray,COLOR_BGR2GRAY);
+    threshold(gray,gray,100,255,cv::THRESH_BINARY);
+    distanceTransform(gray,matDistance,cv::DIST_L2,3);
+        //find the center corners
+    double minVal;
+    double maxVal2;
+    Point minLoc,maxLoc;
+    minMaxLoc( matDistance, &minVal, &maxVal2, &minLoc, &maxLoc, matDistance>0);
+    //draw the result
+    circle(matBoard,maxLoc,1,Scalar(0),1);
+    circle(matBoard,minLoc,2,Scalar(255),2);
+    rectangle(matBoard,Rect(maxLoc.x - 10,maxLoc.y - 10,20,20),Scalar(0,0,255),-1);
+  }
 	return 0;
 }
- 
+
--- example_14-01.cpp
+++ example_14-01.cpp
@@ -6,44 +6,93 @@
 
 using namespace std;
 
-cv::Mat g_gray, g_binary;
+cv::Mat g_gray, g_binary, g_hsv;
 int g_thresh = 100;
 
-void on_trackbar( int, void* ) {
-  cv::threshold( g_gray, g_binary, g_thresh, 255, cv::THRESH_BINARY );
-  vector< vector< cv::Point> > contours;
-  cv::findContours(
-    g_binary,
-    contours,
-    cv::noArray(),
-    cv::RETR_LIST,
-    cv::CHAIN_APPROX_SIMPLE
-  );
-  g_binary = cv::Scalar::all(0);
+void on_trackbar(int, void *)
+{
+    // cv::Mat hsv;
+    // cv::threshold(g_hsv, hsv, g_thresh, 255, cv::THRESH_BINARY);
+    // cv::namedWindow("win1", cv::WINDOW_AUTOSIZE);
+    // cv::imshow("win1", hsv);
+    cv::Mat gray;
+    cv::cvtColor(g_gray, gray, cv::COLOR_RGB2GRAY);
 
-  cv::drawContours( g_binary, contours, -1, cv::Scalar::all(255));
-  cv::imshow( "Contours", g_binary );
+    cv::Mat element = getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7));
 
+    cv::dilate(gray, gray, element);
+
+    cv::Mat bg_img;
+    cv::medianBlur(gray, bg_img, 21);
+    imshow("medianBlur", bg_img);
+
+	// cv::Mat calcMat = ~(closeMat - gray);
+    // imshow("calc", calcMat);
+
+    cv::absdiff(gray, bg_img, gray);
+
+    cv::Mat calcMat = ~(gray);
+    imshow("calc", calcMat);
+
+    cv::Mat removeShadowMat;
+    cv::normalize(calcMat, gray, 0, 255, cv::NORM_MINMAX);
+    imshow("dst", gray);
+
+    // cv::Mat element = getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+    // int iteration = 9;
+
+    // cv::Mat closeMat;
+    // morphologyEx(gray, closeMat, cv::MORPH_CLOSE, element, cv::Point(-1, -1), iteration);
+    // imshow("闭运算", closeMat);
+
+    // cv::Mat calcMat = ~(closeMat - gray);
+    // imshow("calc", calcMat);
+
+    // cv::Mat removeShadowMat;
+    // cv::normalize(calcMat, gray, 0, 255, cv::NORM_MINMAX);
+    // imshow("dst", gray);
+
+    // cv::threshold( gray, g_binary, g_thresh, 255, cv::THRESH_BINARY | cv::THRESH_BINARY_INV);
+    // cv::threshold( g_binary, g_binary, 0, 255, cv::THRESH_BINARY);
+
+    cv::threshold(gray, g_binary, g_thresh, 255, cv::THRESH_BINARY);
+    vector<vector<cv::Point>> contours;
+    cv::findContours(
+        g_binary,
+        contours,
+        cv::noArray(),
+        cv::RETR_TREE,
+        cv::CHAIN_APPROX_NONE);
+    //   g_binary = cv::Scalar::all(0);
+
+    cv::drawContours(g_binary, contours, -1, cv::Scalar::all(255));
+    cv::imshow("Contours", g_binary);
 }
 
-int main( int argc, char** argv ) {
-  if( argc != 2 || ( g_gray = cv::imread(argv[1], 0)).empty() ) {
-    cout << "\nExample 14-1: Find threshold dependent contours\nUsage:\n" <<argv[0]
-         <<" ../fruits.jpg\n" << endl;
-    return -1;
-  }
-  cv::namedWindow( "Contours", 1 );
+int main(int argc, char **argv)
+{
+    if (argc != 2 || (g_gray = cv::imread(argv[1])).empty())
+    {
+        cout << "\nExample 14-1: Find threshold dependent contours\nUsage:\n"
+             << argv[0]
+             << " ../fruits.jpg\n"
+             << endl;
+        return -1;
+    }
 
-  cv::createTrackbar(
-    "Threshold",
-    "Contours",
-    &g_thresh,
-    255,
-    on_trackbar
-  );
-  on_trackbar(0, 0);
+    cv::cvtColor(g_gray, g_hsv, cv::COLOR_RGB2HSV);
 
-  cv::waitKey();
+    cv::namedWindow("Contours", 1);
 
-  return 0;
+    cv::createTrackbar(
+        "Threshold",
+        "Contours",
+        &g_thresh,
+        255,
+        on_trackbar);
+    on_trackbar(0, 0);
+
+    cv::waitKey();
+
+    return 0;
 }
